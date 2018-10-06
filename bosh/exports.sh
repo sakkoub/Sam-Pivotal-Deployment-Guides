333139ee9837eec9f5f61d59ae814b2db5336780
sakkoub <sakkoub@samjumpbox.c.cso-pcfs-apj-sakkoub.internal>
2018-10-06 07:57:13 +0000

    clearing

--- bosh/exports.sh
+++
@@ -1,8 +0,0 @@
-export PROJECT_ID=cso-pcfs-apj-sakkoub
-export VENDOR_PATH=.
-export CREDS_PATH=/.creds
-export GCP_ZONE=australia-southeast1-a
-export GCP_CONTROLPLANE_NETWORK=default
-export GCP_CONTROLPLANE_SUBNET=default 
-#export CONCOURSE_BOSH_DEPLOYMENT=https://github.com/concourse/concourse-bosh-deployment
-#export CONCOURSE_HOST=35.201.12.191 
--- concourse/deploy-concourse-credhub/concourse-credhub/bbl-terraform/aws/concourse-lb_override.tf
+++
@@ -1,9 +0,0 @@
-output "concourse_lb_target_groups" {
-  value = [
-    "${aws_lb_target_group.concourse_lb_80.name}",
-    "${aws_lb_target_group.concourse_lb_443.name}",
-    "${aws_lb_target_group.concourse_lb_2222.name}",
-    "${aws_lb_target_group.concourse_lb_8443.name}",
-    "${aws_lb_target_group.concourse_lb_8844.name}"
-  ]
-}
