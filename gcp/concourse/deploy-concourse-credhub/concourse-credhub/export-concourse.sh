3d709aaf931d963bfdee4c7204e7e3bc49947410
sakkoub <sakkoub@samjumpbox.c.cso-pcfs-apj-sakkoub.internal>
2018-10-06 08:39:20 +0000

    add files cleaned

--- gcp/concourse/deploy-concourse-credhub/concourse-credhub/export-concourse.sh
+++ gcp/concourse/deploy-concourse-credhub/concourse-credhub/export-concourse.sh
@@ -1,10 +1,10 @@
 export CONCOURSE_BOSH_DEPLOYMENT=$PATH_To_CONCOURSE_BOSH_Deployment_Folder
-export CREDHUB_CLIENT=director_to_credhub
-export BOSH_URL= $BOSH_IP
+export CREDHUB_CLIENT=$director_to_credhub
+export BOSH_URL=$BOSH_IP
 
 export CONCOURSE_HOST=$CONCOURSE_HOST_IP
 export CONCOURSE_URL=https://$CONCOURSE_HOST_IP
 
 export CREDHUB_SERVER="$BOSH_URL:8844"
 export BOSH_PATH=$PATH_To_BOSH_BOSH
-export CREDHUB_SECRET="$(bosh int /.creds/creds.yml --path  /uaa_clients_director_to_credhub)"
+export CREDHUB_SECRET="$(bosh int $BOSH_CREDS_PATH/creds.yml --path  /uaa_clients_director_to_credhub)"
